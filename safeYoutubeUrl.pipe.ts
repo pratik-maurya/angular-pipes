e6024048be41168c352d4c3c3fc1e40d5e5002fe
pratik-maurya <98159997+pratik-maurya@users.noreply.github.com>
2023-04-28 00:02:11 +0530

    Create safeYoutubeUrl.pipe.ts

---
+++ safeYoutubeUrl.pipe.ts
@@ -0,0 +1,26 @@
+#Use this pipe for sanitize the youtube Url, to display in angular youtube url bellow safeurl and bind the api response in "iframe" tag.
+==================HTML=============
+<iframe [src]="youtube_url | safeUrl:'resourceUrl'"></iframe>
+
+
+
+
+
+
+===========Pipe=====================
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safeUrl'
+})
+export class SanitizeurlPipe implements PipeTransform {
+  constructor(protected sanitizer: DomSanitizer) {}
+ public transform(value: any, type: string): SafeResourceUrl {
+  let data=value.replace('watch?v=','embed/')
+    switch (type) {
+			case 'resourceUrl': return this.sanitizer.bypassSecurityTrustResourceUrl(data);
+			default: throw new Error(`Invalid safe type specified: ${type}`);
+		}
+  }
+}
